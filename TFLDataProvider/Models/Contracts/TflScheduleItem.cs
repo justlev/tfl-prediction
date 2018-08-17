3149b6f7626eb54ca9923e10d32c2de81502d337
justlev2@gmail.com <justlev2@gmail.com>
2018-08-17 13:56:31 +0300

    started working on tfl connection

---
+++ TFLDataProvider/Models/Contracts/TflScheduleItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TFLDataProvider.Models.Contracts
+{
+    [Serializable]
+    public class TflScheduleItem : ITflScheduledItem
+    {
+        //Those fields are public, for serialisation purposes
+        public string Id;
+        public int OperationType;
+        public string StationName;
+        public string PlatformName;
+        public DateTime ExpectedArrival;
+        public string Towards;
+        public int TimeToStation;
+        
+        
+        // Users will receive the readonly version (interface), so we implement it here.
+        string ITflScheduledItem.Id { get{ return Id; }}
+        int ITflScheduledItem.OperationType { get{ return OperationType; }}
+        string ITflScheduledItem.StationName { get{ return StationName; }}
+        string ITflScheduledItem.PlatformName { get{ return PlatformName; }}
+        DateTime ITflScheduledItem.ExpectedArrival { get{ return ExpectedArrival; }}
+        string ITflScheduledItem.Towards { get{ return Towards; }}
+        int ITflScheduledItem.TimeToStation { get{ return TimeToStation; }}
+    }
+}
---
+++ TFLDataProvider/Models/Contracts/TflStationSchedule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TFLDataProvider.Models.Contracts
+{
+    [Serializable]
+    public class TflStationSchedule : ITflStationSchedule
+    {
+        public List<TflScheduleItem> Schedule = new List<TflScheduleItem>();
+        
+        IEnumerable<ITflScheduledItem> ITflStationSchedule.ScheduledItems
+        {
+            get { return Schedule; }
+        }
+    }
+}

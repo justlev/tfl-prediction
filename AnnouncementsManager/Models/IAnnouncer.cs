e09a94c33096eec135f93032b5123e294ab5e9dc
justlev2@gmail.com <justlev2@gmail.com>
2018-08-17 15:02:03 +0300

    small comment

--- AnnouncementsManager/Models/IAnnouncer.cs
+++ AnnouncementsManager/Models/IAnnouncer.cs
@@ -4,6 +4,7 @@ using TFLDataProvider.Models;
 
 namespace AnnouncementsManager.Models
 {
+    // A better name than IAnnouncer would be in place, since it's repsonsible for scheduling and managing announcements.
     public interface IAnnouncer
     {
         void ScheduleAnnouncements(IEnumerable<ITflScheduledItem> items, Action<ITflScheduledItem> announceAction);
